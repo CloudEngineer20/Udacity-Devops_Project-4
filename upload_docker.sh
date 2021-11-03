4890ed3172ed7bd2818b262e323ce02ecc31a17a
CloudEngineer20 <71173913+CloudEngineer20@users.noreply.github.com>
2021-11-04 02:38:54 +0300

    Create upload_docker.sh

---
+++ upload_docker.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+# dockerpath=<your docker ID/path>
+dockerpath=geek30/excellent21
+
+# Step 2:  
+# Authenticate & tag
+docker login
+docker image tag udacity $dockerpath
+echo "Docker ID and Image: $dockerpath"
+
+
+
+# Step 3:
+# Push image to a docker repository
+docker image push $dockerpath
